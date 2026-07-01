f35b05682bdc8a5d66d16d14e4ed3f83896290c1
aminsoheyli <aminsoheyli98@gmail.com>
2026-07-01 19:48:27 +0330

    add post model and declare get all /posts route

--- full-api-development/main.py
+++ full-api-development/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Response, status
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: int | None = None
+
+
+my_posts = [
+    Post(title='title of post 1', content='content of post 1').model_dump(exclude_unset=True) | {'id': 1},
+    Post(title='favorite foods', content='I like pizza').model_dump(exclude_unset=True) | {'id': 2},
+]
 
 
 @app.get("/")
 async def root():
     return {"data": "Welcome to my API!"}
+
+
+@app.get('/posts')
+def get_posts():
+    return {"data": my_posts}
