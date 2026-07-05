0e7805e45311b6fdf94c28e70798158fd5d8e199
aminsoheyli <aminsoheyli98@gmail.com>
2026-07-05 06:30:32 +0330

    refactor rename body column to content

--- full-api-development/app/models.py
+++ full-api-development/app/models.py
@@ -7,5 +7,5 @@ class Post(Base):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True, not_null=True)
     title = Column(VARCHAR(255), nullable=False)
-    body = Column(VARCHAR(255), nullable=False)
+    content = Column(VARCHAR(255), nullable=False)
     published = Column(Boolean, nullable=False, server_default='true')
