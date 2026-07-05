90df98180c16efd2f65efc148fc549e0d9d9a8fd
aminsoheyli <aminsoheyli98@gmail.com>
2026-07-05 06:59:37 +0330

    chore: add created_at to the Post model

--- full-api-development/app/models.py
+++ full-api-development/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, VARCHAR, Boolean
+from sqlalchemy import Column, Integer, VARCHAR, Boolean, TIMESTAMP, text
 
 from .database import Base
 
@@ -9,3 +9,4 @@ class Post(Base):
     title = Column(VARCHAR(255), nullable=False)
     content = Column(VARCHAR(255), nullable=False)
     published = Column(Boolean, nullable=False, server_default='true')
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
