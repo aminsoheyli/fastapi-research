9b01fe77a63dd41c17a18b1784116612c30a12eb
aminsoheyli <aminsoheyli98@gmail.com>
2026-07-05 04:23:38 +0330

    add database_url property to Settings class

--- full-api-development/app/config.py
+++ full-api-development/app/config.py
@@ -5,14 +5,22 @@ class Settings(BaseSettings):
     postgres_user: str
     postgres_password: str
     postgres_db: str
-    postgres_host: str = "localhost"
+    postgres_host: str
     postgres_port: int = 5432
+    debug: bool = False
 
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore"
     )
+
+    @property
+    def database_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
+            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+        )
 
 
 settings = Settings()
