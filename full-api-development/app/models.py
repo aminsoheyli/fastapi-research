26b45cb6606aaa829766ba6bb306fc35c61835ac
aminsoheyli <aminsoheyli98@gmail.com>
2026-07-06 19:38:59 +0330

    fix: remove nullable property from Post model's id column

--- full-api-development/app/models.py
+++ full-api-development/app/models.py
@@ -5,7 +5,7 @@ from .database import Base
 
 class Post(Base):
     __tablename__ = 'posts'
-    id = Column(Integer, primary_key=True, not_null=True)
+    id = Column(Integer, primary_key=True)
     title = Column(VARCHAR(255), nullable=False)
     content = Column(VARCHAR(255), nullable=False)
     published = Column(Boolean, nullable=False, server_default='true')
