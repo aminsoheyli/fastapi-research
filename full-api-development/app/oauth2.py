3e24653bb98e5d8783753e1d24777cfbb6f85691
aminsoheyli <aminsoheyli98@gmail.com>
2026-07-07 17:31:24 +0330

    chore: add verify access token function and get current user function

--- full-api-development/app/oauth2.py
+++ full-api-development/app/oauth2.py
@@ -1,14 +1,47 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
 
 import jwt
+from fastapi import HTTPException, status
+from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordBearer
 
+from app import schemas
 from app.config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=settings.jwt_access_token_expire_minutes)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.jwt_access_token_expire_minutes)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, settings.jwt_secret_key, algorithm=settings.jwt_algorithm)
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, settings.jwt_secret_key, algorithms=[settings.jwt_algorithm])
+        user_id = payload.get('user_id')
+        if user_id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(user_id=user_id)
+    except jwt.PyJWTError:
+        raise credentials_exception
+
+    return token_data
+
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    return verify_access_token(token, credentials_exception)
+
+
+GetCurrentUserDep = Annotated[int, Depends(get_current_user)]
