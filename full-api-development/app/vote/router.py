7c71b60fc561e0f3ed895cccce39c8091c68e84e
aminsoheyli <aminsoheyli98@gmail.com>
2026-07-07 23:25:46 +0330

    chore: add vote router

---
+++ full-api-development/app/vote/router.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, HTTPException, status
+from sqlalchemy import select
+
+from app import schemas, models
+from app.database import SessionDep
+from app.oauth2 import GetCurrentUserDep
+
+router = APIRouter(prefix='/vote', tags=['Vote'])
+
+
+@router.post('/')
+async def login(vote: schemas.Vote, session: SessionDep, user: GetCurrentUserDep):
+    result = await session.execute(
+        select(models.Vote).where(models.Vote.post_id == vote.post_id, models.Vote.user_id == user.id)
+    )
+    found_vote = result.scalars().first()
+    if vote.dir == 1:
+        if found_vote:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f'You have already voted on post {vote.post_id}'
+            )
+        new_vote = models.Vote(post_id=vote.post_id, user_id=user.id)
+        session.add(new_vote)
+        await session.commit()
+        return {'message': 'successfully added vote'}
+    else:
+        if not found_vote:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='Vote does not exist'
+            )
+        await session.delete(found_vote)
+        await session.commit()
+        return {'message': 'successfully deleted vote'}
