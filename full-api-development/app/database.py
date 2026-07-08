de1fda6bd2b3d76116fa36fad5bf532604325cf7
aminsoheyli <aminsoheyli98@gmail.com>
2026-07-08 07:02:14 +0330

    chore: remove comments

--- full-api-development/app/database.py
+++ full-api-development/app/database.py
@@ -28,12 +28,3 @@ async def get_session():
 
 
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
-
-# async def get_session():
-#     async with async_session() as session:
-#         try:
-#             yield session
-#             await session.commit()
-#         except Exception:
-#             await session.rollback()
-#             raise
